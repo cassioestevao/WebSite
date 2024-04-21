aeaffc43c5995731e248a5007619089f6c6e755c
Cássio Estevão Pereira dos Santos <116197477+cassioestevao@users.noreply.github.com>
2024-04-21 14:45:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+menuIcon.onclick = ()=>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}

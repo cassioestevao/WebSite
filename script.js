46969eced80d3535bab884ae10d694ecc5bb72c3
Cássio Estevão Pereira dos Santos <116197477+cassioestevao@users.noreply.github.com>
2024-04-22 10:51:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    
+}
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
